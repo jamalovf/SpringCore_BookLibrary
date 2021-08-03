869e31918aa5c2385052a9d7630465adbe35af90
Filip Jamalov <filip.jamalov@endava.com>
2021-08-03 18:17:37 +0200

    Creating beans, accessing them and coding the main method

--- src/main/java/com/example/springcore/Configuration/AuthorConfig.java
+++ src/main/java/com/example/springcore/Configuration/AuthorConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AuthorConfig {
 
+
     @Bean
     public Author authorConfig(){
         return new Author();
